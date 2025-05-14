68d2bcdb344ca3ad8bc4a8c616772864c6ad2814
Betul Yaman <betul.iyican@icredible.com>
2025-05-14 16:06:55 +0300

    Handle the flag FILE_DELETE_ON_CLOSE

--- delete/minifilter.c
+++ delete/minifilter.c
@@ -102,6 +102,11 @@ NTSTATUS register_filter(_In_ PDRIVER_OBJECT driver_object)
          pre_operation_callback,
 		 NULL},
 
+        {IRP_MJ_CREATE,
+         0,
+         pre_create_operation_callback,
+         NULL},
+
         {IRP_MJ_OPERATION_END}
     };
 
@@ -299,6 +304,34 @@ FLT_PREOP_CALLBACK_STATUS pre_operation_callback(
             return FLT_PREOP_COMPLETE;
         }
         return FLT_PREOP_PENDING;
+    }
+
+    return FLT_PREOP_SUCCESS_NO_CALLBACK;
+}
+
+
+
+FLT_PREOP_CALLBACK_STATUS pre_create_operation_callback(
+    _Inout_ PFLT_CALLBACK_DATA data,
+    _In_ PCFLT_RELATED_OBJECTS filter_objects,
+    _Flt_CompletionContext_Outptr_ PVOID* completion_callback
+) {
+    LOG_MSG("pre_operation_callback START");
+
+    UNREFERENCED_PARAMETER(filter_objects);
+    UNREFERENCED_PARAMETER(completion_callback);
+
+    if (data->Iopb->MajorFunction == IRP_MJ_CREATE) {
+        ULONG createOptions = data->Iopb->Parameters.Create.Options;
+
+        if ((createOptions & FILE_DELETE_ON_CLOSE) == FILE_DELETE_ON_CLOSE) {
+            DbgPrint("MiniFilter: FILE_DELETE_ON_CLOSE flag detected.\n");
+
+            // You can block it like this:
+            data->IoStatus.Status = STATUS_ACCESS_DENIED;
+            data->IoStatus.Information = 0;
+            return FLT_PREOP_COMPLETE;
+        }
     }
 
     return FLT_PREOP_SUCCESS_NO_CALLBACK;
--- delete/minifilter.h
+++ delete/minifilter.h
@@ -49,4 +49,9 @@ FLT_PREOP_CALLBACK_STATUS pre_operation_callback(
 	_Flt_CompletionContext_Outptr_ PVOID* completion_context
 );
 
+FLT_PREOP_CALLBACK_STATUS pre_create_operation_callback(
+	_Inout_ PFLT_CALLBACK_DATA data,
+	_In_ PCFLT_RELATED_OBJECTS filter_objects,
+	_Flt_CompletionContext_Outptr_ PVOID* completion_callback
+);
 #endif //KERNEL_DELETE_MINIFILTER_H
